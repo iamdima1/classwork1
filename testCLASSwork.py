77ad2449f5dce62f2be427cedeeb696f323d9069
iamdima1 <90511126+iamdima1@users.noreply.github.com>
2021-09-19 12:46:30 +0300

    change 1work.py

--- testCLASSwork.py
+++ testCLASSwork.py
@@ -1,3 +1,80 @@
-print "hello World!"
+# my_value = 7
+# my_bool = my_value % 3 == 0
+# my_bool1 = my_value % 2 ==0
+# print(not my_bool and not my_bool1)
+
+
+
+
+
+# a= 'Hello,"A"'
+# b= a
+# c= 'Hello,"B"'
+# b= c
+# c= a
+# print(a)
+
+
+
+
+
+# valueX= 2
+# valueY= '4'
+#
+# valueX= str(valueX)
+#
+# # valueZ= 1
+# valueU= print(valueX + valueY)
+
+
+
+
+
+
+# приведения типов
+#
+# value= "-19"
+# new_value = float(value)
+# print(new_value, type(new_value))
+
+
+# some_value= 4
+# bool_value_1 = (2 != some_value)
+# bool_value = bool_value_1
+
+#print(bool_value, type(bool_value))
+
+
+
+
+
+
+# value_1 = "25"
+# value_2 = "2"
+#
+#
+# #value= value_1 % value_2
+# #value= value_1 ** value_2
+# value= value_1 + value_2
+#
+# print(value)
+
+
+# value = "100"
+# value = 100
+# first = "qwe"
+# second = 1
+# value = first
+# first = second
+# second = value
+#
+# print(first, second)
+
+
+# print ("hello World!")
 # comment
-print "hello World!"
+# print ("hello World!")
+
+# print (123 * 2, 456, "789", "qwerty")
+# print (12 - 3)
+# print (23 + 5)
